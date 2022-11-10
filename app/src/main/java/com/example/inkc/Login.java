2de3dd37553a1577aa202d8b2bca7f434c64c8fe
Manjeet <manjeetrajbhar89@gmail.com>
2022-11-10 15:53:21 +0530

    done

--- app/src/main/java/com/example/inkc/Login.java
+++ app/src/main/java/com/example/inkc/Login.java
@@ -60,7 +60,7 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initialize(savedInstanceState);
-        initializeLogic();
+      //  initializeLogic();
 
 
     }
@@ -114,72 +114,72 @@ public class Login extends AppCompatActivity {
 
 
 
-    public void initializeLogic() {
-        _view(Login, 50, 13, "#42AF5F");
-        _view(button3, 50, 13, "#42A5F5");
-        _view(linear_login, 50, 13, "#FFFFFF");
-    }
-
-    public void _view(final View _view, final double _radius, final double _shadow, final String _color) {
-        android.graphics.drawable.GradientDrawable ab = new android.graphics.drawable.GradientDrawable();
-
-        ab.setColor(Color.parseColor(_color));
-        ab.setCornerRadius((float) _radius);
-        _view.setElevation((float) _shadow);
-        _view.setBackground(ab);
-    }
-
-
-    @Deprecated
-    public void showMessage(String _s) {
-        Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
-    }
-
-    @Deprecated
-    public int getLocationX(View _v) {
-        int _location[] = new int[2];
-        _v.getLocationInWindow(_location);
-        return _location[0];
-    }
-
-    @Deprecated
-    public int getLocationY(View _v) {
-        int _location[] = new int[2];
-        _v.getLocationInWindow(_location);
-        return _location[1];
-    }
-
-    @Deprecated
-    public int getRandom(int _min, int _max) {
-        Random random = new Random();
-        return random.nextInt(_max - _min + 1) + _min;
-    }
-
-    @Deprecated
-    public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
-        ArrayList<Double> _result = new ArrayList<Double>();
-        SparseBooleanArray _arr = _list.getCheckedItemPositions();
-        for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
-            if (_arr.valueAt(_iIdx))
-                _result.add((double)_arr.keyAt(_iIdx));
-        }
-        return _result;
-    }
-
-    @Deprecated
-    public float getDip(int _input) {
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
-    }
-
-    @Deprecated
-    public int getDisplayWidthPixels() {
-        return getResources().getDisplayMetrics().widthPixels;
-    }
-
-    @Deprecated
-    public int getDisplayHeightPixels() {
-        return getResources().getDisplayMetrics().heightPixels;
-    }
+//    public void initializeLogic() {
+//        _view(Login, 50, 13, "#91f288");
+//        _view(button3, 50, 13, "#91f288");
+//        _view(linear_login, 50, 13, "#FFFFFF");
+//    }
+//
+//    public void _view(final View _view, final double _radius, final double _shadow, final String _color) {
+//        android.graphics.drawable.GradientDrawable ab = new android.graphics.drawable.GradientDrawable();
+//
+//        ab.setColor(Color.parseColor(_color));
+//        ab.setCornerRadius((float) _radius);
+//        _view.setElevation((float) _shadow);
+//        _view.setBackground(ab);
+//    }
+//
+//
+//    @Deprecated
+//    public void showMessage(String _s) {
+//        Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Deprecated
+//    public int getLocationX(View _v) {
+//        int _location[] = new int[2];
+//        _v.getLocationInWindow(_location);
+//        return _location[0];
+//    }
+//
+//    @Deprecated
+//    public int getLocationY(View _v) {
+//        int _location[] = new int[2];
+//        _v.getLocationInWindow(_location);
+//        return _location[1];
+//    }
+//
+//    @Deprecated
+//    public int getRandom(int _min, int _max) {
+//        Random random = new Random();
+//        return random.nextInt(_max - _min + 1) + _min;
+//    }
+//
+//    @Deprecated
+//    public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+//        ArrayList<Double> _result = new ArrayList<Double>();
+//        SparseBooleanArray _arr = _list.getCheckedItemPositions();
+//        for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+//            if (_arr.valueAt(_iIdx))
+//                _result.add((double)_arr.keyAt(_iIdx));
+//        }
+//        return _result;
+//    }
+//
+//    @Deprecated
+//    public float getDip(int _input) {
+//        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+//    }
+//
+//    @Deprecated
+//    public int getDisplayWidthPixels() {
+//        return getResources().getDisplayMetrics().widthPixels;
+//    }
+//
+//    @Deprecated
+//    public int getDisplayHeightPixels() {
+//        return getResources().getDisplayMetrics().heightPixels;
+//    }
 
     @SuppressLint("NotConstructor")
     public void Login() {
